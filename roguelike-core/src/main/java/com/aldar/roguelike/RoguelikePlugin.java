b1896e36b45b459642f9473111c90fa3e20e8970
devdynam0507 <wsnam0507@naver.com>
2023-02-02 03:25:37 +0900

    feat: initial commit

---
+++ roguelike-core/src/main/java/com/aldar/roguelike/RoguelikePlugin.java
@@ -0,0 +1,11 @@
+package com.aldar.roguelike;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class RoguelikePlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+}
