0acfcd6ebf9c1426aa2fa324f644ef14d3bdc2de
devdynam0507 <wsnam0507@naver.com>
2023-03-08 01:01:44 +0900

    feat: world 관련 코드 수정:

--- roguelike-core/src/main/java/com/aldar/roguelike/RoguelikePlugin.java
+++ roguelike-core/src/main/java/com/aldar/roguelike/RoguelikePlugin.java
@@ -1,13 +1,16 @@
 package com.aldar.roguelike;
 
+import org.bukkit.World;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.aldar.roguelike.world.VoidWorldGenerator;
 
 public class RoguelikePlugin extends JavaPlugin {
 
+    private World roguelikeWorld;
+
     @Override
     public void onEnable() {
-        VoidWorldGenerator.generateEmptyWorld("roguelike_world", this);
+        this.roguelikeWorld = VoidWorldGenerator.generateEmptyWorld("roguelike_world", this);
     }
 }
--- roguelike-core/src/main/java/com/aldar/roguelike/world/VoidWorldGenerator.java
+++ roguelike-core/src/main/java/com/aldar/roguelike/world/VoidWorldGenerator.java
@@ -7,7 +7,7 @@ import org.bukkit.plugin.Plugin;
 
 public class VoidWorldGenerator {
 
-    public static void generateEmptyWorld(final String worldName, final Plugin plugin) {
+    public static World generateEmptyWorld(final String worldName, final Plugin plugin) {
        final WorldCreator worldCreator = new WorldCreator(worldName);
        worldCreator.type(WorldType.FLAT);
        worldCreator.generateStructures(false);
@@ -15,5 +15,6 @@ public class VoidWorldGenerator {
        if (createdWorld != null) {
            plugin.getServer().getWorlds().add(createdWorld);
        }
+       return createdWorld;
     }
 }
